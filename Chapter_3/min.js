98a8e951c0d1de6576ade03bafa8ff03b22053cf
dienigma <joshi.chinmay09@gmail.com>
2019-03-31 13:27:16 +0530

    A function to return the minimum of the two arguments, passed to it

---
+++ Chapter_3/min.js
@@ -0,0 +1,11 @@
+// This function returns the min argument of the two.
+
+function min(argumentOne, argumentTwo){
+    return argumentOne > argumentTwo ? argumentTwo : argumentOne
+}
+
+// Driver Code
+
+console.log(min(2,3))
+console.log(min("a",3))
+console.log(min("A","a"))
