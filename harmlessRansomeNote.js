4d748ff0c6eb3e60981217ae69cb406ea4ab50a1
dienigma <joshi.chinmay09@gmail.com>
2019-04-03 16:09:50 +0530

    Finished the second half of the problem and tested the code

--- harmlessRansomeNote.js
+++ harmlessRansomeNote.js
@@ -10,6 +10,19 @@ function harmlessRansomNote(noteText, magazineText) {
         magazineObj[word]++
     });
 
-    console.log(magazineObj)
+    var noteIsPossible = true
+    noteArr.forEach(word => {
+        if(magazineObj[word]){
+            magazineObj[word]--
+            if (magazineObj[word] < 0){
+                noteIsPossible = false
+            }
+        } else {
+            noteIsPossible = false
+        }
+    })
+
+    return noteIsPossible;
 }
 
+console.log(harmlessRansomNote('this is a secret note from your secret admirer','eiusmod admirer tempor incididunt secret ut labore et this dolore is a magna aliqua note from your secret '))
