aa8287308c64cc4d7c81bc5c4810a2dd58d36074
dienigma <joshi.chinmay09@gmail.com>
2019-04-05 11:56:10 +0530

    finished setting up bootstrap starterpack

--- BootStrap/bs4Projects/bs4starter/gulpfile.js
+++ BootStrap/bs4Projects/bs4starter/gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('sass', function(){
 // Move Js to scripts.js
 
 gulp.task('js', function (){
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_mdules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest('src/js'))
         .pipe(browserSync.stream())
 })
