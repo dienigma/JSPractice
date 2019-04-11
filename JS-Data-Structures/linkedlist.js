29933ea97c725301b4c97c3a244974c6c4ff7e4f
dienigma <joshi.chinmay09@gmail.com>
2019-04-11 18:17:54 +0530

    Finished up on add to tail mehtod.

--- JS-Data-Structures/linkedlist.js
+++ JS-Data-Structures/linkedlist.js
@@ -15,3 +15,11 @@ LinkedList.prototype.addToHead = function (value){
     else this.tail = newNode
     this.head = newNode
 }
+
+LinkedList.prototype.addToTail = function (value){
+    var newNode = new Node(value,null,this.tail)
+    if(this.tail) this.tail.next = newNode
+    else this.head = newNode
+    this.tail = newNode
+}
+
