c2f536238e97fd94b720072d4a5ee3bc751a9bb3
dienigma <joshi.chinmay09@gmail.com>
2019-04-11 17:36:00 +0530

    created constructors for ll and node

---
+++ JS-Data-Structures/linkedlist.js
@@ -0,0 +1,11 @@
+function LinkedList(){
+    this.head = null;
+    this.tail = null;
+};
+
+function Node(value, next, previous){
+    this.value = value;
+    this.next = next;
+    this.previous = previous;
+};
+
