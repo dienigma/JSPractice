f859b3f9c2da8223b60572933c32e9a213da613f
dienigma <joshi.chinmay09@gmail.com>
2019-04-11 17:46:45 +0530

    wrote the AddToHead method

--- JS-Data-Structures/linkedlist.js
+++ JS-Data-Structures/linkedlist.js
@@ -1,11 +1,17 @@
 function LinkedList(){
-    this.head = null;
-    this.tail = null;
-};
+    this.head = null
+    this.tail = null
+}
 
 function Node(value, next, previous){
-    this.value = value;
-    this.next = next;
-    this.previous = previous;
-};
+    this.value = value
+    this.next = next
+    this.previous = previous
+}
 
+LinkedList.prototype.addToHead = function (value){
+    var newNode = new Node(value, this.head, null)
+    if(this.head) this.head.previous = newNode
+    else this.tail = newNode
+    this.head = newNode
+}
